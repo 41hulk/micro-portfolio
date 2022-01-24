15e870ddbe1226c024a8f34c23d2123a21aa421d
Ntare Guy <gntare2@gmail.com>
2022-01-24 23:18:51 +0200

    Added event on menu

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const btn = document.querySelector('#menu-btn');
+const closeButton = document.querySelector('#close-btn');
+
 function display() {
-    console.log("====================HELLOWORLD===========");
+    const menu = document.querySelector('#menu');
+    menu.classList.add('menu-visible');
+}
+function close(){
+    const menu = document.querySelector('#menu');
+    menu.classList.remove('menu-visible');
 }
 
 btn.addEventListener('click',display);
+closeButton.addEventListener('click',close);
