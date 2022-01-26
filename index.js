f906bceac9585f9aa571a97634923250336a531c
Ntare Guy <gntare2@gmail.com>
2022-01-26 16:53:36 +0200

    Add script

---
+++ index.js
@@ -0,0 +1,13 @@
+const project = [
+    {
+        image:"./image/pic.jpeg",
+        title:"Multi-Post Stories Gain+Glory",
+        language: ['HTML5','HTML5','HTML5',]
+    }
+]
+
+for(let i=0;i<=6;i += 1){
+    const card = document.createElement('div');
+    card.classList.add();
+
+}
