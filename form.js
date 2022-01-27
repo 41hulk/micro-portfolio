c6ad6941e1dec6a28b85c42e93c941cb10c132d1
Ntare Guy <gntare2@gmail.com>
2022-01-27 18:33:18 +0200

    fix linters issues

--- form.js
+++ form.js
@@ -1,22 +1,18 @@
 function validateEmail(form) {
-  // check if email is written properly '_replyto'
-  const email = form.elements['_replyto'].value
-  
-  //  to check if email was written only using lowercase char
-  const emailRegex = /^([a-z0-9_-]+)@([a-z0-9_-]+).((([a-z]){3})|((([a-z]){3}).(([a-z]){1,3})))$/
-  
-  return emailRegex.test(email)
+  // eslint-disable-next-line no-underscore-dangle
+  const email = form.elements._replyto.value;
+  const emailRegex = /^([a-z0-9_-]+)@([a-z0-9_-]+).((([a-z]){3})|((([a-z]){3}).(([a-z]){1,3})))$/;
+  return emailRegex.test(email);
 }
 
-// get the form => add Event listner to submit
 const contactForm = document.forms[0];
 
 contactForm.addEventListener('submit', (event) => {
   const checkEmail = validateEmail(contactForm);
 
-  if(!checkEmail) {
+  if (!checkEmail) {
     const errorMsg = document.querySelector('.error-msg');
-    errorMsg.style.display = 'block'
-    event.preventDefault()
+    errorMsg.style.display = 'block';
+    event.preventDefault();
   }
 });
